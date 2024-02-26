5904a611ea5e6b2260bbf6a5290439acdd070966
ReinierSoares <reiniersoaresberthier@gmail.com>
2024-02-25 23:25:00 -0300

    minor corrections

--- resources/views/sattelite/edit.blade.php
+++ resources/views/sattelite/edit.blade.php
@@ -5,19 +5,19 @@
 <div class="container mt-5">
     <h1>Edit a Satellite</h1>
     <hr>
-    <form action="{{ route('satellites-update',['id'=>$satellites->id])}}" method="POST">
+    <form action="{{ route('satellites-update',['id'=>$sattelites->id])}}" method="POST">
         @csrf
         @method('PUT')
         <div class="form-group">
             <div class="form-group">
                 <label for="name">Name:</label>
-                <input type="text" class="form-control" name="name" value="{{$satellites->name}}">
+                <input type="text" class="form-control" name="name" value="{{$sattelites->name}}">
                 <br>
                 <label for="year">Year:</label>
-                <input type="number" class="form-control" name="year" value="{{$satellites->year}}">
+                <input type="number" class="form-control" name="year" value="{{$sattelites->year}}">
                 <br>
                 <label for="value">Value:</label>
-                <input type="number" class="form-control" name="value" value="{{$satellites->value}}">
+                <input type="number" class="form-control" name="value" value="{{$sattelites->value}}">
                 <br>
                 <div class="form-group">
                     <input type="submit" name="submit" class="btn btn-success" value="submit">
