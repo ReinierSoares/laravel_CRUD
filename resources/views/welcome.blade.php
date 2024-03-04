ffdb710948ad2310a85e2dfc9263a8de7a2878dc
ReinierSoares <reiniersoaresberthier@gmail.com>
2024-03-04 00:56:45 -0300

    href

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -843,7 +843,7 @@
             <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
             @endif
             @endauth -->
-            <a href="{{route('satellites-index')}}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Satellites</a>
+            <a href="{{ route('satellites-index') }}">Satellites</a>
         </div>
         <!-- @endif -->
 
